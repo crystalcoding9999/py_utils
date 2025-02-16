0a4e36645786c2486c2de9ebd1306a5057404599
crystalcoding9999 <crystalcraft9999@gmail.com>
2025-02-16 17:06:27 +0100

    [fix]: few printing fixes
    details:
    - fix that os prints error when in an IDE terminal
    - made error messages easier to read by adding seperated lines

--- __init__.py
+++ __init__.py
@@ -24,19 +24,36 @@ def __try_colorama_import():
 
 # import warnings
 __empty_terminal_os_IMPORT_WARNING = False
+__empty_terminal_NO_TERM_WARNING = False
 __color_print_colorama_IMPORT_WARNING = False
 
 # utils
+
+# terminal utils
+
 def clear_terminal():
     empty_terminal()
 def empty_terminal():
     os = __try_os_import()
-    global __empty_terminal_os_IMPORT_WARNING
+    global __empty_terminal_os_IMPORT_WARNING, __empty_terminal_NO_TERM_WARNING
     if os is None:
         if not __empty_terminal_os_IMPORT_WARNING:
+            print("")
             print("The function `empty_terminal()` requires the `os` module")
             print("Install the `os` module and try again")
+            print("")
             __empty_terminal_os_IMPORT_WARNING = True
+
+        return
+
+    if "TERM" not in os.environ:
+        if not __empty_terminal_NO_TERM_WARNING:
+            print("")
+            print("The function `empty_terminal()` does not work in this terminal environment!")
+            print("This is usually caused by running in a IDE terminal")
+            print("Try running the script directly from the terminal to resolve this issue.")
+            print("")
+            __empty_terminal_NO_TERM_WARNING = True
 
         return
 
@@ -47,12 +64,14 @@ def color_print(color, *args):
     global __color_print_colorama_IMPORT_WARNING
     if colorama is None:
         if not __color_print_colorama_IMPORT_WARNING:
+            print("")
             print("The function `color_print()` requires the 'colorama` module")
             print("Install the `colorama` module and try again")
+            print("")
             __color_print_colorama_IMPORT_WARNING = True
 
         # functionality available without colorama
-        s = "".join(map(str, args))
+        s = " ".join(map(str, args))
         print(s)
 
         return
@@ -60,6 +79,6 @@ def color_print(color, *args):
     if color not in colorama.Fore or color is None:
         color = ""
 
-    s = "".join(map(str, args))
+    s = " ".join(map(str, args))
 
     print(color + s + colorama.Fore.RESET)
